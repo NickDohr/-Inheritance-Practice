28218c98d3f3b1bd289488a095ed79ed8d9c4204
nicholas.dohr <nicholas.dohr@MU-C1-09.mullen.pvt.k12.co.us>
2022-05-23 13:28:20 -0600

    Inheritance Practice

---
+++ Inheritance Practice/src/RunnerClass.java
@@ -0,0 +1,16 @@
+
+public class RunnerClass
+	{
+		public static void main(String[] args)
+		{
+		//Vehicle x = new Vehicle();
+	//	Vehicle x = new Bicycle();
+		Bicycle x = new Bicycle();
+//		Bicyle x = new Vehicle();
+		
+		x.displaySpeed();
+		x.hitPedestrian();
+		x.ringBell();
+		
+		}
+	}
---
+++ Inheritance Practice/src/Vehicle.java
@@ -0,0 +1,9 @@
+
+public abstract class Vehicle
+	{
+		private String type = "vehicle";
+		public void displaySpeed()
+			{System.out.println("not fast enough");}
+		public void hitPedestrian()
+		{System.out.println("Splat!");}
+	}
